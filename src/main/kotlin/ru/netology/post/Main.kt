f3f643129eb0fb69781577198752a1237022bb4d
vanya <whitebeltvanya@gmail.com>
2025-10-01 23:39:57 +0300

    add attachment logic thanks to jomarzka

--- src/main/kotlin/ru/netology/post/Main.kt
+++ src/main/kotlin/ru/netology/post/Main.kt
@@ -1,5 +1,6 @@
 package ru.netology.post
 
+
 data class Post(
     val id: Int = 0,
     val ownerId: Int = 0,
@@ -12,7 +13,7 @@ data class Post(
     val views: Views? = null,
     val likes: Likes? = null,
     val reposts: Reposts? = null,
-    var attachments: Array<Attachment> = emptyArray<Attachment>()
+    var attachments: Set<Attachment> = mutableSetOf()
 )
 
 data class Views(val counts: Int)
@@ -27,47 +28,85 @@ enum class AttachmentType {
     FOTO,
     AUDIO,
     VIDEO,
-    DOC,
-    LINK,
-    NOTE
+    FILE,
+    STICKER
 }
 
 interface Attachment {
     val type: AttachmentType
 }
 
-data class AttachFoto (
-    val id: Int =0,
-    val ownerId: Int = 0,
-    val albumId: Int =0,
-    val userId: Int = 0,
-    val width: Int = 0,
-    val height: Int = 0,
-): Attachment{
+data class FotoAttachment(
+    val foto: Foto
+) : Attachment {
     override val type: AttachmentType = AttachmentType.FOTO
 }
 
-data class AttachAudio (
-    val id: Int =0,
+data class Foto(
+    val id: Int = 0,
     val ownerId: Int = 0,
-    val artist: String ="",
-    val title: String = "",
-    val duration: Int = 0,
-    val url: String = ""
-): Attachment{
+    val albumId: Int = 0,
+    val userId: Int = 0,
+    val width: Int = 0,
+    val height: Int = 0
+)
+
+data class AudioAttachment(
+    val audio: Audio
+) : Attachment {
     override val type: AttachmentType = AttachmentType.AUDIO
 }
 
-data class AttachVideo (
-    val id: Int =0,
+data class Audio(
+    val id: Int = 0,
+    val ownerId: Int = 0,
+    val artist: String = "",
+    val title: String = "",
+    val duration: Int = 0,
+    val url: String = ""
+)
+
+data class VideoAttachment(
+    val video: Video
+) : Attachment {
+    override val type: AttachmentType = AttachmentType.VIDEO
+}
+
+data class Video(
+    val id: Int = 0,
     val ownerId: Int = 0,
     val title: String = "",
     val description: String = "",
     val duration: Int = 0,
-): Attachment{
-    override val type: AttachmentType = AttachmentType.VIDEO
+)
+
+data class FileAttachment(
+    val file: File
+) : Attachment {
+    override val type: AttachmentType = AttachmentType.FILE
 }
 
+data class File(
+    val id: Int = 0,
+    val ownerId: Int = 0,
+    val title: String = "",
+    val size: Int = 0,
+    val ext: String = "",
+    val url: String = ""
+)
+
+data class StickerAttachment(
+    val sticker: Sticker
+) : Attachment {
+    override val type: AttachmentType = AttachmentType.STICKER
+}
+
+data class Sticker(
+    val productId: Int = 0,
+    val stickerId: Int = 0,
+    val animationUrl: String = "",
+    val isAllowed: Boolean = true
+)
 
 object WallService {
 
@@ -90,6 +129,16 @@ object WallService {
         return false
     }
 
+    fun addAttachment(post: Post, attachment: Attachment): Boolean {
+        for ((index, postChecked) in posts.withIndex()) {
+            if (postChecked.id == post.id) {
+                post.attachments += attachment
+                return true
+            }
+        }
+        return false
+    }
+
     fun getNewId(): Int = ++newId //thanks to jomarzka
     fun getPostsCounts() = posts.size
     fun clear() {
@@ -104,6 +153,9 @@ fun main() {
 
     println(wallService.add(post))
     println("All posts counts: ${wallService.getPostsCounts()}")
+
+    val newVideoAttach = VideoAttachment(Video(title = "New video"))
+    wallService.addAttachment(post, newVideoAttach)
 
     val newPost = Post(id = 3, text = "New Text")
     if (wallService.update(newPost)) {
